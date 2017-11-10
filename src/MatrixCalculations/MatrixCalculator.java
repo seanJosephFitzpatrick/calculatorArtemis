5608304c3107f04958cb2b2e17204982be2e6b66
Dean Galvin <a00248144@student.ait.ie>
2017-11-10 17:00:06 +0000

    Added Addition, Subtraction and Multiplication of Double and Int matrices

--- src/MatrixCalculations/MatrixCalculator.java
+++ src/MatrixCalculations/MatrixCalculator.java
@@ -1,5 +1,8 @@
 package MatrixCalculations;
 
+import java.security.InvalidParameterException;
+import java.text.NumberFormat;
+
 public class MatrixCalculator {
 
 	static int[][] testA =
@@ -14,6 +17,7 @@ public class MatrixCalculator {
 		{5, 9, 3, 1},
 		{1, 6, 6, 7}};
 
+	//MULTIPLICATION - DOUBLE
 	public double[][] multiplyMatrix(double[][] A, double[][] B) {
 		int aRows = A.length;
 		int aColumns = A[0].length;
@@ -43,6 +47,7 @@ public class MatrixCalculator {
 		return C;
 	}
 
+	//MULTIPLICATION - INT
 	public int[][] multiplyMatrix(int[][] A, int[][] B) {
 		int aRows = A.length;
 		int aColumns = A[0].length;
@@ -72,8 +77,143 @@ public class MatrixCalculator {
 		return C;
 	}
 
+	//ADDITION - DOUBLE
+	public double[][] addMatrix(double[][] A, double[][] B) {
+		int aRows = A.length;
+		int aColumns = A[0].length;
+		int bRows = B.length;
+		int bColumns = B[0].length;
+
+		double[][] C = new double[aRows][bColumns];
+
+		if (aRows != bRows) {
+			throw new IllegalArgumentException("Rows not equal size");
+		}
+		if (aColumns != bColumns) {
+			throw new IllegalArgumentException("Columns not equal size");
+		}
+
+		for (int r = 0; r < aRows; r++) {
+			for (int c = 0; c < bColumns; c++) {
+				C[r][c] = 0;
+			}
+		}
+
+		for (int r = 0; r < aRows; r++) {
+			for (int c = 0; c < bColumns; c++) {
+				C[r][c] = A[r][c] + B[r][c];
+			}
+		}
+
+		return C;
+	}
+
+	//ADDITION - INT
+	public int[][] addMatrix(int[][] A, int[][] B) {
+		int aRows = A.length;
+		int aColumns = A[0].length;
+		int bRows = B.length;
+		int bColumns = B[0].length;
+
+		int[][] C = new int[aRows][bColumns];
+
+		if (aRows != bRows) {
+			throw new IllegalArgumentException("Rows not equal size");
+		}
+		if (aColumns != bColumns) {
+			throw new IllegalArgumentException("Columns not equal size");
+		}
+
+		for (int r = 0; r < aRows; r++) {
+			for (int c = 0; c < bColumns; c++) {
+				C[r][c] = 0;
+			}
+		}
+
+		for (int r = 0; r < aRows; r++) {
+			for (int c = 0; c < bColumns; c++) {
+				C[r][c] = A[r][c] + B[r][c];
+			}
+		}
+
+		return C;
+	}
+
+	//SUBTRACTION - DOUBLE
+	public double[][] subtractMatrix(double[][] A, double[][] B) {
+		int aRows = A.length;
+		int aColumns = A[0].length;
+		int bRows = B.length;
+		int bColumns = B[0].length;
+
+		double[][] C = new double[aRows][bColumns];
+
+		if (aRows != bRows) {
+			throw new IllegalArgumentException("Rows not equal size");
+		}
+		if (aColumns != bColumns) {
+			throw new IllegalArgumentException("Columns not equal size");
+		}
+
+		for (int r = 0; r < aRows; r++) {
+			for (int c = 0; c < bColumns; c++) {
+				C[r][c] = 0;
+			}
+		}
+
+		for (int r = 0; r < aRows; r++) {
+			for (int c = 0; c < bColumns; c++) {
+				C[r][c] = A[r][c] - B[r][c];
+			}
+		}
+
+		return C;
+	}
+
+	//SUBTRACTION - INT
+		public int[][] subtractMatrix(int[][] A, int[][] B) {
+			int aRows = A.length;
+			int aColumns = A[0].length;
+			int bRows = B.length;
+			int bColumns = B[0].length;
+
+			int[][] C = new int[aRows][bColumns];
+
+			if (aRows != bRows) {
+				throw new IllegalArgumentException("Rows not equal size");
+			}
+			if (aColumns != bColumns) {
+				throw new IllegalArgumentException("Columns not equal size");
+			}
+
+			for (int r = 0; r < aRows; r++) {
+				for (int c = 0; c < bColumns; c++) {
+					C[r][c] = 0;
+				}
+			}
+
+			for (int r = 0; r < aRows; r++) {
+				for (int c = 0; c < bColumns; c++) {
+					C[r][c] = A[r][c] - B[r][c];
+				}
+			}
+
+			return C;
+		}
+
+	public static void printArray(int[][] toPrint){
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(3);
+		for(int i = 0; i < toPrint.length; i++){
+			for(int j = 0; j < toPrint[0].length; j++){
+				System.out.print(nf.format(toPrint[i][j]) + "\t");
+			}
+			System.out.println();
+		}
+	}
+
 	public static void main(String[] args) {
 		MatrixCalculator m = new MatrixCalculator();
-		m.multiplyMatrix(testA, testB);
+		printArray(m.addMatrix(testA, testB));
 	}
 }
