ee3c1ea4a14ebe6655a0d62a8b4c4b23046b3e37
oskar <oskar@DESKTOP-BG0AKEU>
2017-11-11 18:32:07 +0000

    Buttons and textfields setUp

--- src/GUI/FXMLMatrixCalculationController.java
+++ src/GUI/FXMLMatrixCalculationController.java
@@ -1,5 +1,185 @@
 package GUI;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import ReversePolishNotation.CalcInterface;
+import ReversePolishNotation.EquationGenerator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
 public class FXMLMatrixCalculationController {
+	   @FXML
+	    private Button buttonGenerateMatrix1;
+
+	    @FXML
+	    private TextField textFieldMatrix101;
+
+	    @FXML
+	    private TextField textFieldMatrix102;
+
+	    @FXML
+	    private TextField textFieldMatrix103;
+
+	    @FXML
+	    private TextField textFieldMatrix105;
+
+	    @FXML
+	    private TextField textFieldMatrix106;
+
+	    @FXML
+	    private TextField textFieldMatrix107;
+
+	    @FXML
+	    private TextField textFieldMatrix104;
+
+	    @FXML
+	    private TextField textFieldMatrix108;
+
+	    @FXML
+	    private TextField textFieldMatrix112;
+
+	    @FXML
+	    private TextField textFieldMatrix116;
+
+	    @FXML
+	    private TextField textFieldMatrix115;
+
+	    @FXML
+	    private TextField textFieldMatrix111;
+
+	    @FXML
+	    private TextField textFieldMatrix110;
+
+	    @FXML
+	    private TextField textFieldMatrix109;
+
+	    @FXML
+	    private TextField textFieldMatrix114;
+
+	    @FXML
+	    private TextField textFieldMatrix113;
+
+	    @FXML
+	    private TextField textFieldMatrix201;
+
+	    @FXML
+	    private TextField textFieldMatrix202;
+
+	    @FXML
+	    private TextField textFieldMatrix203;
+
+	    @FXML
+	    private TextField textFieldMatrix204;
+
+	    @FXML
+	    private TextField textFieldMatrix205;
+
+	    @FXML
+	    private TextField textFieldMatrix206;
+
+	    @FXML
+	    private TextField textFieldMatrix207;
+
+	    @FXML
+	    private TextField textFieldMatrix208;
+
+	    @FXML
+	    private TextField textFieldMatrix209;
+
+	    @FXML
+	    private TextField textFieldMatrix210;
+
+	    @FXML
+	    private TextField textFieldMatrix211;
+
+	    @FXML
+	    private TextField textFieldMatrix212;
+
+	    @FXML
+	    private TextField textFieldMatrix213;
+
+	    @FXML
+	    private TextField textFieldMatrix214;
+
+	    @FXML
+	    private TextField textFieldMatrix215;
+
+	    @FXML
+	    private TextField textFieldMatrix216;
+
+	    @FXML
+	    private Button buttonGenerateMatrix2;
+
+	    @FXML
+	    private TextField textFieldMatrix301;
+
+	    @FXML
+	    private TextField textFieldMatrix302;
+
+	    @FXML
+	    private TextField textFieldMatrix303;
+
+	    @FXML
+	    private TextField textFieldMatrix304;
+
+	    @FXML
+	    private TextField textFieldMatrix305;
+
+	    @FXML
+	    private TextField textFieldMatrix306;
+
+	    @FXML
+	    private TextField textFieldMatrix307;
+
+	    @FXML
+	    private TextField textFieldMatrix308;
+
+	    @FXML
+	    private TextField textFieldMatrix309;
+
+	    @FXML
+	    private TextField textFieldMatrix310;
+
+	    @FXML
+	    private TextField textFieldMatrix311;
+
+	    @FXML
+	    private TextField textFieldMatrix312;
+
+	    @FXML
+	    private TextField textFieldMatrix313;
+
+	    @FXML
+	    private TextField textFieldMatrix314;
+
+	    @FXML
+	    private TextField textFieldMatrix315;
+
+	    @FXML
+	    private TextField textFieldMatrix316;
+
+	    @FXML
+	    private Button buttonAddMatrix;
+
+	    @FXML
+	    private Button buttonSubtractMatrix;
+
+	    @FXML
+	    private Button buttonDivideMatrix;
+
+	    @FXML
+	    private Button buttonMultiplyMatrix;
 
 }
