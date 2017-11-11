a24acd0023cd396d3a5afb8653badb2a39237a7e
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2017-11-11 03:02:18 +0000

    Added menu navigation

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,29 +1,61 @@
 package GUI;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
+/**
+ * Main application class.
+ */
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			//BorderPane root = new BorderPane();
-			Parent root = FXMLLoader.load(getClass().getResource("FXMLEquationCalc.fxml"));
-			Scene scene = new Scene(root,800,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setTitle("Applied Mathematics Project");
+
+        stage.setScene(
+            createScene(
+                loadMainPane()
+            )
+        );
+
+        stage.show();
+    }
+
+    private Pane loadMainPane() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+
+        Pane mainPane = (Pane) loader.load(
+            getClass().getResourceAsStream(
+                WindowNavigator.MAIN
+            )
+        );
+
+        FXMLMainPaneController mainController = loader.getController();
+
+        WindowNavigator.setMainController(mainController);
+        WindowNavigator.loadPane(WindowNavigator.BASIC_CALCULATOR);
+
+        return mainPane;
+    }
+    
+    private Scene createScene(Pane mainPane) {
+        Scene scene = new Scene(
+            mainPane
+        );
+
+        scene.getStylesheets().setAll(
+            getClass().getResource("application.css").toExternalForm()
+        );
+
+        return scene;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
