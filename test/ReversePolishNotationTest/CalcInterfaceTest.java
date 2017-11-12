ca9900b7494840268d731d554bcf85a9074c5f7f
KieranMcM <10111778@studentmail.ul.ie>
2017-11-12 21:24:16 +0000

    Unit tests added to calculator

--- test/ReversePolishNotationTest/CalcInterfaceTest.java
+++ test/ReversePolishNotationTest/CalcInterfaceTest.java
@@ -5,79 +5,114 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import ReversePolishNotation.CalcInterface;
+import ReversePolishNotation.EquationGenerator;
 
 public class CalcInterfaceTest {
 
 	@Test
 	public void testTwoNumbersAdditionOK() {
-		assertEquals("9.0", CalcInterface.run("4+5", true));
+		assertEquals("9", CalcInterface.run("4+5", true));
 	}
 	
 	@Test
 	public void testTwoNumbersSubtractionOK() {
-		assertEquals("-2.0", CalcInterface.run("4-6", true));
+		assertEquals("-2", CalcInterface.run("4-6", true));
 	}
 
 	@Test
 	public void testTwoNumbersMultiplicationOK() {
-		assertEquals("35.0", CalcInterface.run("7*5", true));
+		assertEquals("35", CalcInterface.run("7*5", true));
 	}
 	
 	@Test
 	public void testTwoNumbersDivisionOK() {
-		assertEquals("2.0", CalcInterface.run("10/5", true));
+		assertEquals("2", CalcInterface.run("10/5", true));
 	}
 	
 	@Test
 	public void testNumberRaisedToPowerOK() {
-		assertEquals("64.0", CalcInterface.run("2^(2+4)", true));
+		assertEquals("64", CalcInterface.run("2^(2+4)", true));
 	}
 	
 	@Test
 	public void testOneBracketExpressionOK() {
-		assertEquals("100.0", CalcInterface.run("10 * ( 5 + 5 )", true));
+		assertEquals("100", CalcInterface.run("10 * ( 5 + 5 )", true));
 	}
 	
 	@Test
 	public void testTwoBracketExpressionOK() {
-		assertEquals("100.0", CalcInterface.run("(20 / 2) * ( 5 + 5 )", true));
+		assertEquals("100", CalcInterface.run("(20 / 2) * ( 5 + 5 )", true));
 	}
 	
 	@Test
-	public void testCosFunctionOK() {
-		assertEquals("0.5000000000000001", CalcInterface.run("cos(60)", true));			//Clean double!
+	public void testCosFunctionTrueOK() {
+		assertEquals("0.500000000000", CalcInterface.run("cos(60)", true));		
 	}
 	
 	@Test
-	public void testSinFunctionOK() {
-		assertEquals("0.8660254037844386", CalcInterface.run("sin(60)", true));			
+	public void testCosFunctionFalseOK() {
+		assertEquals("-0.952412980415", CalcInterface.run("cos(60)", false));		
+	}
+	
+	@Test
+	public void testACosFunctionTrueOK() {
+		assertEquals("60.0000000000", CalcInterface.run("bac(0.5)", true));		
+	}
+	
+	@Test
+	public void testACosFunctionFalseOK() {
+		assertEquals("1.04719755120", CalcInterface.run("bac(0.5)", false));		
+	}
+	
+	@Test
+	public void testSinFunctionTrueOK() {
+		assertEquals("0.866025403784", CalcInterface.run("sin(60)", true));			
+	}
+	
+	@Test
+	public void testSinFunctionFalseOK() {
+		assertEquals("-0.304810621102", CalcInterface.run("sin(60)", false));			
+	}
+	
+	@Test
+	public void testASinFunctionTrueOK() {
+		assertEquals("30.0000000000", CalcInterface.run("dsn(0.5)", true));			
+	}
+	
+	@Test
+	public void testASinFunctionFalseOK() {
+		assertEquals("0.523598775598", CalcInterface.run("dsn(0.5)", false));			
 	}
 	
 	@Test
 	public void testTanFunctionOK() {
-		assertEquals("1.7320508075688767", CalcInterface.run("tan(60)", true));			
+		assertEquals("1.73205080757", CalcInterface.run("tan(60)", true));			
 	}
 	
 	@Test
 	public void testSquareRootFunctionOK() {
-		assertEquals("3.0", CalcInterface.run("rts(9)", true));
+		assertEquals("3", CalcInterface.run("rts(9)", true));
 	}
 	
 	@Test
 	public void testNaturalLogFunction() {
-		assertEquals("2.1972245773362196", CalcInterface.run("elg(9)", true));
+		assertEquals("2.19722457734", CalcInterface.run("elg(9)", true));
 	}
 	
 	@Test
 	public void testLogTenFunction() {
-		assertEquals("0.9542425094393249", CalcInterface.run("lgt(9)", true));
+		assertEquals("0.954242509439", CalcInterface.run("lgt(9)", true));
 	}
 	
 	@Test
 	public void testFunctionWithinBracketsOK() {
-		assertEquals("6.866025403784438", CalcInterface.run("1+(5+cos(30))", true));
+		assertEquals("6.86603", CalcInterface.run("1+(5+cos(30))", true));
 	}
 		
+	@Test
+	public void testFactorial(){
+		assertEquals("1 + 120", EquationGenerator.evaluateFactorial("1 + 5!"));
+	}
 
 	
 	
